82f57ab4d54cd71ae641ebaa26d756cc50b5c879
AYOUBA <nj.ayou95@gmail.com>
2024-01-24 21:22:27 +0000

    initial commit

---
+++ src/main/java/com/ecommerceapp/ecommerceapp/HelloController.java
@@ -0,0 +1,14 @@
+package com.ecommerceapp.ecommerceapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
